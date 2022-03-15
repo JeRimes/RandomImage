76bd11355816252fe29e8306dccd92ffb176f759
Jeremy <jeremy@yasserkidiwe.onmicrosoft.com>
2022-03-15 15:08:30 +0100

    add locomotive scroll

---
+++ pages/fav.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+import Link from 'next/link'
+export default function Home() {
+
+    useEffect(() => {
+        let id = JSON.parse(localStorage.getItem("id"));
+        console.log(id);
+    }, []);
+
+    return (
+        <div>
+            <h1>Vos images favorites</h1>
+            <Link href="/">
+                <a>Go to home</a>
+            </Link>
+        </div>
+
+
+    )
+
+
+
+}
+
+// export async function getServerSideProps(context) {
+//     // Fetch data from external API
+//     const res = await fetch(`https://picsum.photos/id/`)
+//     const data = await res.json()
+//     // Pass data to the page via props
+//     return { props: { data } }
+// }
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,55 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { useState, useEffect } from 'react'
+import React from 'react';
+import { useState, useEffect, useRef } from 'react'
+import Link from 'next/link'
+
 export default function Home({ data }) {
-
+  console.log(data);
   const [idImage, setIdImage] = useState([]);
+  const refScrollContainer = useRef(null);
 
-  const handleSubmit = (event) => {
-    const data = new FormData(event.currentTarget)
-    event.preventDefault();
-
+  const AddFav = (idInput) => {
     //check if exist
-    let exist = idImage.find(item => item.id === data.get('idImage'));
+    let exist = idImage.find(item => item.id === idInput);
     if (!exist || idImage.length == 0) {
-      let item = { id: data.get('idImage') }
+      let item = { id: idInput }
       setIdImage([...idImage, item]);
     }
-
-
   };
+  const RemoveFav = (idInput) => {
+    //check if exist
+    let exist = idImage.find(item => item.id === idInput);
+    if (exist) {
+      setIdImage(idImage.filter(item => item.id != idInput));
+    }
+  };
+
+  useEffect(() => {
+    async function getLocomotive() {
+      const Locomotive = (await import("locomotive-scroll")).default;
+      new Locomotive({
+        el: refScrollContainer.current,
+        direction: 'horizontal',
+        reloadOnContextChange: true,
+        touchMultiplier: 2,
+        smoothMobile: 0,
+        multiplier: 1.4,
+        smooth: true,
+        smartphone: {
+          smooth: true,
+          breakpoint: 767
+        },
+        tablet: {
+          smooth: true,
+          breakpoint: 1024
+        },
+      });
+    }
+
+    getLocomotive();
+  }, []);
 
   useEffect(() => {
     // storing input name
@@ -26,7 +57,6 @@ export default function Home({ data }) {
     console.log(JSON.parse(localStorage.getItem("id")));
 
   }, [idImage]);
-
 
   return (
     <div className={styles.container}>
@@ -36,36 +66,39 @@ export default function Home({ data }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main className="main" ref={refScrollContainer} data-scroll-container>
+        <Link href="/fav" className="">
+          <a>My collection</a>
+        </Link>
+
         {
           data.map((img) =>
-            <div key={img.id}>
-              <Image src={img.download_url} alt={img.author} width={img.width} height={img.height}></Image>
-              <form onSubmit={handleSubmit}>
-                <input
-                  type="text"
-                  value={img.id}
-                  name="idImage"
-                  readOnly
-                />
-                <input type="submit" value="Add this image"></input>
-              </form>
+            <div key={img.id} className="card-container" data-scroll-section>
+              <div className="card-image" >
+                <Image
+                  className='random-image'
+                  src={img.download_url + ".webp"}
+                  alt={img.author}
+                  width={600}
+                  height={450}
+
+                  data-scroll
+                  data-scroll-speed="2"
+
+                ></Image>
+                <button onClick={() => AddFav(img.id)}>Add</button>
+                <button onClick={() => RemoveFav(img.id)}>Remove</button>
+              </div>
             </div>
           )
         }
+
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        <p>
+          Made by passion ©JeRimes
+        </p>
       </footer>
     </div>
   )
@@ -73,7 +106,7 @@ export default function Home({ data }) {
 
 export async function getServerSideProps() {
   // Fetch data from external API
-  const res = await fetch(`https://picsum.photos/v2/list?page=2&limit=1`)
+  const res = await fetch(`https://picsum.photos/v2/list?limit=10`)
   const data = await res.json()
   // Pass data to the page via props
   return { props: { data } }
