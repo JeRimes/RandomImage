a1ef5027e347346e037ef2e02e1c0abee6893807
Jeremy <jeremy@yasserkidiwe.onmicrosoft.com>
2022-03-16 15:39:42 +0100

    save for deploy

--- pages/fav.js
+++ pages/fav.js
@@ -1,11 +1,24 @@
 import { useState, useEffect } from 'react'
 import Link from 'next/link'
-export default function Home() {
+
+export default function Fav() {
+    const [isLoading, setIsLoading] = useState(false)
+    const [saveImage, setsaveImage] = useState([])
 
     useEffect(() => {
         let id = JSON.parse(localStorage.getItem("id"));
         console.log(id);
+
+        setIsLoading(true)
+        fetch('https://picsum.photos/id/0/info')
+            .then(response => response.json())
+            .then(obj => {
+                console.log(obj);
+                setsaveImage([...saveImage, obj]);
+            })
     }, []);
+
+
 
     return (
         <div>
@@ -13,6 +26,10 @@ export default function Home() {
             <Link href="/">
                 <a>Go to home</a>
             </Link>
+            {saveImage.map((img) =>
+                <h1 key={img.author}>{img.author}</h1>
+            )
+            }
         </div>
 
 
